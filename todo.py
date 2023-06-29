eeace00557899661cf31f87f110f428d4de0b065
XID-IvanMP <ivan.molina@xid.com.mx>
2023-06-29 14:25:50 -0600

    T2: Actualizar una tarea específica

--- todo.py
+++ todo.py
@@ -9,12 +9,27 @@ class TodoList:
         for task in self.tasks:
             print(task)
 
+    def update_task(self):
+        print("Elige la tarea que deseas modificar:")
+        self.view_tasks()
+
+        place = input("Ingresa el número de tarea: ")
+
+        if int(place) -1 > len(self.tasks):
+            print("La tarea elegida no existe.")
+            return
+
+        task = input("Ingresa el nuevo contenido de la tarea: ")
+
+        self.tasks[int(place) - 1] = task
+
 def main():
     todo_list = TodoList()
     while True:
         print("1. Añadir tarea")
         print("2. Ver tareas")
-        print("3. Salir")
+        print("3. Actualizar tarea")
+        print("4. Salir")
         option = input("Seleccione una opción: ")
         if option == "1":
             task = input("Ingrese la tarea: ")
@@ -22,10 +37,12 @@ def main():
         elif option == "2":
             todo_list.view_tasks()
         elif option == "3":
+            todo_list.update_task()
+        elif option == "4":
             break
         else:
             print("Opción no válida. Inténtalo de nuevo.")
 
 
 if __name__ == "__main__":
-main()
+    main()
